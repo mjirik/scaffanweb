d374abcd208207097ac96933be683c99dd3a521d
Miroslav Jirik <miroslav.jirik@gmail.com>
2020-06-24 13:10:49 +0200

    wsgi app log

--- scaffanweb/wsgi.py
+++ scaffanweb/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scaffanweb.settings')
 
+print("we are in python now")
 application = get_wsgi_application()
+print("after get_wsgi_application()")
