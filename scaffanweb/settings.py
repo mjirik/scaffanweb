f1c3390f1b468fb9aa6ce398da356feae2253c08
Miroslav Jirik <miroslav.jirik@gmail.com>
2020-06-27 07:08:30 +0200

    just static

--- scaffanweb/settings.py
+++ scaffanweb/settings.py
@@ -149,7 +149,8 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = "static"
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, "static"),
 # )
