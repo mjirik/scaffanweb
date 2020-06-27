27db2663900312434e2617d4eeff75a375f773bc
Miroslav Jirik <miroslav.jirik@gmail.com>
2020-06-27 07:05:00 +0200

    STATIC_ROOT instead of STATICFILES_DIRS

--- scaffanweb/settings.py
+++ scaffanweb/settings.py
@@ -149,9 +149,10 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 
 
 AUTHENTICATION_BACKENDS = (
