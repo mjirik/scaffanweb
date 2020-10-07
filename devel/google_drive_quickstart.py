fcf416a65776487e44b274c6b0b14c1726c6e149
Miroslav Jirik <miroslav.jirik@gmail.com>
2020-10-07 19:48:39 +0200

    download works

--- devel/google_drive_quickstart.py
+++ devel/google_drive_quickstart.py
@@ -8,15 +8,19 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from googleapiclient.http import MediaIoBaseDownload
+import io
+from pathlib import Path
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly', 'https://www.googleapis.com/auth/drive.readonly']
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly', 'https://www.googleapis.com/auth/drive.readonly', 'https://www.googleapis.com/auth/drive.file']
 
 def main():
     """Shows basic usage of the Drive v3 API.
     Prints the names and ids of the first 10 files the user has access to.
     """
     creds = None
+    dfilespath = "download"
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
@@ -42,12 +46,14 @@ def main():
         driveId=driveId,
         # q="name = '11_2019_11_13__-7.czi'",
         # q="mimeType = 'application/vnd.google-apps.folder'", # list all directories
-        q="'1pStkl9_vEQJHTAc0OIbP4X39GmcBhVBJ' in parents", # all files in Moulisova-Jena
+        # q="'1pStkl9_vEQJHTAc0OIbP4X39GmcBhVBJ' in parents", # all files in Moulisova-Jena
+        q="'1OsKfZlp_s6RPHXXei8LsZunTNjPsBwMb' in parents", # all files in scaffan_import
         includeItemsFromAllDrives=True,
         supportsAllDrives=True,
         corpora="drive",
         pageSize=10, fields="nextPageToken, files(id, name)").execute()
     items = results.get('files', [])
+
 
     if not items:
         print('No files found.')
@@ -55,6 +61,22 @@ def main():
         print('Files:')
         for item in items:
             print(u'{0} ({1})'.format(item['name'], item['id']))
+
+        file_id = item["id"]
+        name = item['name']
+        drive_service = service
+        request = drive_service.files().get_media(fileId=file_id)
+        fh = io.BytesIO()
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print("Download %d%%." % int(status.progress() * 100))
+
+        Path(dfilespath).mkdir(parents=True, exist_ok=True)
+        with io.open(dfilespath + "/" + name, 'wb') as f:
+            fh.seek(0)
+            f.write(fh.read())
 
 if __name__ == '__main__':
     main()
