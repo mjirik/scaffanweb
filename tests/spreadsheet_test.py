2b8fe82980ebd3c1fd86ffa8837335a49ebe79c1
Miroslav Jirik <miroslav.jirik@gmail.com>
2022-06-24 10:58:24 +0200

    request-efficient spreadsheet update

---
+++ tests/spreadsheet_test.py
@@ -0,0 +1,28 @@
+import pytest
+# importing the required libraries
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+from pathlib import Path
+from typing import Union
+from microimprocessing.data_tools import google_spreadsheet_append
+
+
+
+def test_spreadsheet():
+    creds_file = Path('piglegsurgery-1987db83b363.json')
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name(creds_file, scope)
+
+    novy = {"Prvni sloupec": ["aa"], "Treti": [55]}
+    novy = {"loupec": ["aa"]}
+    df_novy = pd.DataFrame(novy)
+
+    google_spreadsheet_append(
+        title="Pigleg Surgery Stats",
+        creds=creds,
+        data=df_novy
+    )
+
+
+
