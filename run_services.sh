40062bc022994914f48c8896557f40a1d0b83598
Miroslav Jirik <miroslav.jirik@gmail.com>
2023-01-12 21:46:53 +0100

    typo fix

--- run_services.sh
+++ run_services.sh
@@ -7,6 +7,6 @@ echo "Starting services..."
 #sudo service redis-server start &>> logs/redis_log.txt
 service redis-server start |& tee -a logs/redis_log.txt &
 echo "  Redis started"
-conda run --no-capture-output -n scaffanweb python manage.py qcluster |& tee -a logs/qculster_log.txt &
+conda run --no-capture-output -n scaffanweb python manage.py qcluster |& tee -a logs/qcluster_log.txt &
 conda run --no-capture-output -n scaffanweb python manage.py runserver 0.0.0.0:8000 |& tee -a logs/runserver_log.txt &
 echo "Services started"
