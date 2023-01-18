2ce970ba99aaa06a18f0109b419e27d82a4da35d
Miroslav Jirik <mjirik@ntis.zcu.cz>
2023-01-18 11:04:56 +0100

    run on linux

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -d -v "C:/Users/Jirik/projects/scaffanweb:/webapps/scaffanweb_django/scaffanweb/" -v "C:/Users/Jirik/projects/scaffan:/webapps/scaffanweb_django/scaffan/" -p 8000:8000 -p 8080:80 --name scaffan scaffan:0.1
+
+
+docker run -d -v "/home/mjirik/projects/scaffanweb:/webapps/scaffanweb_django/scaffanweb/" -v "/home/mjirik/projects/scaffan:/webapps/scaffanweb_django/scaffan/" -p 80:8000 -p 443:443 --name scaffan scaffan:0.1
