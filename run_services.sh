d06c314d0e1fe9f385e9f21f0a96c0349f423ed0
Miroslav Jirik <mjirik@ntis.zcu.cz>
2023-09-13 13:33:21 +0200

    different logging

--- run_services.sh
+++ run_services.sh
@@ -8,7 +8,8 @@ echo "Starting services..."
 service redis-server start |& tee -a logs/redis_log.txt &
 echo "  Redis started"
 conda run --no-capture-output -n scaffanweb python manage.py qcluster |& tee -a logs/qcluster_log.txt &
-conda run --no-capture-output -n scaffanweb gunicorn scaffanweb.wsgi:application --bind 0.0.0.0:8000 --log-level=debug |& tee -a logs/runserver_gunicorn_log.txt &
+conda run --no-capture-output -n scaffanweb gunicorn scaffanweb.wsgi:application --bind 0.0.0.0:8000 --log-level=debug &
+# conda run --no-capture-output -n scaffanweb gunicorn scaffanweb.wsgi:application --bind 0.0.0.0:8000 --log-level=debug |& tee -a logs/runserver_gunicorn_log.txt &
 # conda run --no-capture-output -n scaffanweb python manage.py runserver 0.0.0.0:8000 |& tee -a logs/runserver_log.txt &
 echo "Services started"
 
